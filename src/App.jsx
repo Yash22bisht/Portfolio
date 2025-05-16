931fd6714981c093a22862b71b9f8de944face17
Yash Bisht <yashbisht22.05@gmail.com>
2025-05-16 17:34:08 +0530

    Refactor App component layout and improve structure with Navbar integration

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import {Navbar} from "./components/navbar.jsx";
+import { Navbar } from "./components/navbar.jsx";
 
 function App() {
   return (
     <div >
-      <div className=" h-screen w-screen bg-[#14001B]">
-        <Navbar/>
+      <div className="relative h-screen w-screen pt-7 pl-24 bg-[#14001B]">
+        <div class="absolute top-1/2 left-3/4 -translate-x-1/2 -translate-y-1/2 
+              w-[600px] h-[600px] rounded-full 
+              bg-[#85148A] opacity-50 blur-[150px] z-0">
+        </div>
+        <Navbar />
+
+        <div className="cont w-full h-full flex">
+          <div className="leftCont w-1/2 h-full ">
+            <h2 className='text-white'>welcome to my world</h2>
+          </div>
+          <div className="rightCont w-1/2 h-full ">
+
+          </div>
+        </div>
+
       </div>
     </div>
   )
